141692cf0fd0ce5975d873dafcfebf0005ceb228
Life Light <lifelightdev@gmail.com>
2025-04-28 14:33:29 +0200

    Add test print date

--- src/main/java/ExtractInfo.java
+++ src/main/java/ExtractInfo.java
@@ -2,4 +2,14 @@ public class ExtractInfo {
     public static String syndicName(String ligne) {
         return ligne.replace("|","");
     }
+
+    public static String printDate(String line) {
+        String[] words = line.trim().split(" ");
+        for (String word : words) {
+            if (word.matches("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(19|20)\\d{2}$")){
+                return word;
+            }
+        }
+        return "";
+    }
 }
--- src/test/java/ExtractInfoTest.java
+++ src/test/java/ExtractInfoTest.java
@@ -13,4 +13,15 @@ public class ExtractInfoTest {
         String name = ExtractInfo.syndicName(line);
         Assertions.assertEquals(SyndicName, name);
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "8 AVENUE DES CHAMPS ELYSE 11/04/2025 Page : 1,  11/04/2025",
+            "8 AVENUE DES CHAMPS ELYSE 14/04/2025 Page : 10, 14/04/2025"
+    })
+    public void extractPrintDate(String line, String printDate) {
+        String date = ExtractInfo.printDate(line);
+        Assertions.assertEquals(printDate, date);
+    }
+
 }
